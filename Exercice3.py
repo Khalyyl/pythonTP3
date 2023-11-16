7f906084151480458b9c0aa878f6a6b19bc76571
Khalyyl <khalilaskri588@gmail.com>
2023-11-16 20:44:50 +0100

    first commit

--- Exercice3.py
+++ Exercice3.py
@@ -8,6 +8,7 @@ liste=[]
 for i in range(0,taille_liste):
     x=int(input(f'Donner liste[{i}]'))
     liste.append(x)
+    
 print(liste)
 print("La somme de la liste :",sum(liste[::]))
 print("Min liste",min(liste))
