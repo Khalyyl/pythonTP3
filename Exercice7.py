19394793cd2e551e1cfb1e3967abea2d69a02671
Khalyyl <khalilaskri588@gmail.com>
2023-11-16 22:40:56 +0100

    first commit

---
+++ Exercice7.py
@@ -0,0 +1,11 @@
+ch="un bon étudiant est celui qui révise chaque jour son cours"
+ch1 = ch.split(' ')[1]
+print(ch1)
+
+# un bon bonbon
+ch2 = " ".join(ch.split(" ")[0:2]) +" "+ (ch.split(" ")[1]*2) 
+print(ch2)
+
+#cours
+ch3 = ch.split(" ")[-1]
+print(ch3)
